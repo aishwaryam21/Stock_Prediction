16dbafb50daff567d1337632df08f7d28e13cea8
aishwaryam <89262891+aishwaryam21@users.noreply.github.com>
2022-02-05 19:41:16 +0530

    Create Prediction.py

---
+++ Prediction.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('marketprophecy/BTC USD/BTC-USD Training Data - 1st Jan 2016 to 1st Jan 2022.csv')
+dataset.head()
